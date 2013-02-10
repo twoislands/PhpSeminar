7a161db358b12e26d073cf1db97ab8b931fd81ad
twoislands <dionisiosk@gmail.com>
2013-02-10 16:58:04 +0200

    Initial part for this assignment

---
+++ part1_number_comparison.php
@@ -0,0 +1,19 @@
+<!********************************************
+   SCRIBBLER:	TwoIslandS
+   WEBSITE:	http://twoislands.net
+   VERSION:	1.0 beta
+ *****************************************-->
+
+<html>
+<head><title>Εργασία 3</title><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<body>
+<h1>Assignment 3</h1>
+<h2>Exercise 1</h2>
+<h3>Comparison of two numbers</h3>
+
+<?php
+
+?>
+</body>
+</html>
